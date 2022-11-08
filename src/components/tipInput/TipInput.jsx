262aae2fe3a9df938cc8c0851a5f6889843680ed
Miguel Villanueva <emvil542@gmail.com>
2022-11-08 23:48:56 +0800

    feat(tip input): add styling
    fix(UI): fix font sizes

--- src/components/tipInput/TipInput.jsx
+++ src/components/tipInput/TipInput.jsx
@@ -1,7 +1,15 @@
 const TipInput = () => {
     return(
-        <div>
-            TipInput
+        <div className="tip">
+            <label  className="tip__label">Select Tip %</label>
+            <section className="tip__options">
+                <option className="tip__option">5%</option>
+                <option className="tip__option">10%</option>
+                <option className="tip__option">15%</option>
+                <option className="tip__option">25%</option>
+                <option className="tip__option">50%</option>
+                <input className="tip__option tip__option--custom" type="number" placeholder="Custom"/>
+            </section>
         </div>
     )
 }
