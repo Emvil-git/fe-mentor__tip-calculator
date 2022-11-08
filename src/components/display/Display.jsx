f64629b06244e362b1ed7b49b4c419842827eddf
Miguel Villanueva <emvil542@gmail.com>
2022-11-08 20:31:28 +0800

    feat(display): add styling

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,21 +1,23 @@
 const Display = () => {
     return(
         <div className="display">
-            <section className="display__wrap">
-                <section className="display__labels">
-                    <label className="display__label-amt">Tip Amount</label>
-                    <label className="display__label-person">/ person</label>
+            <section>
+                <section className="display__wrap">
+                    <section className="display__labels">
+                        <label className="display__label-amt">Tip Amount</label>
+                        <label className="display__label-person">/ person</label>
+                    </section>
+                    <label className="display__value">$4.27</label>
                 </section>
-                <label className="display__value">$4.27</label>
-            </section>
-            <section className="display__wrap">
-                <section className="display__labels">
-                    <label className="display__label-amt">Total</label>
-                    <label className="display__label-person">/ person</label>
+                <section className="display__wrap">
+                    <section className="display__labels">
+                        <label className="display__label-amt">Total</label>
+                        <label className="display__label-person">/ person</label>
+                    </section>
+                    <label className="display__value">$4.27</label>
                 </section>
-                <label className="display__value">$4.27</label>
             </section>
-            <button>RESET</button>
+            <button className="display__reset">RESET</button>
         </div>
     )
 }
