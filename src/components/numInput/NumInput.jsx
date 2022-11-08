88c904d758ddd6221ef450d5f2f403ce73b4dc25
Miguel Villanueva <emvil542@gmail.com>
2022-11-08 21:11:31 +0800

    feat(num input): add styling

--- src/components/numInput/NumInput.jsx
+++ src/components/numInput/NumInput.jsx
@@ -1,10 +1,12 @@
+import { PersonFill, CurrencyDollar } from 'react-bootstrap-icons';
+
 const NumInput = () => {
     return(
-        <div>
-            <label>Bill</label>
-            <section>
-                
-                <input type="text" />
+        <div className='input'>
+            <label id='input__label'>Bill</label>
+            <section className='input__grp'>
+                <CurrencyDollar className='input__icon'/>
+                <input placeholder='0' type="number" className='input__box'/>
             </section>
         </div>
     )
